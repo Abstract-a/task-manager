aa13517f435111d7eeea9aef83e86e2bc21ac0d9
Abstract-a <m.kamari100@gmail.com>
2024-01-07 19:13:13 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const dotenv = require('dotenv/config');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/tasks', require('./routes/taskRoutes'));
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
